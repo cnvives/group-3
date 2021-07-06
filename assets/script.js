b027ed0173b5f39b37e400ac87384c976f54010d
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-07-06 14:22:41 -0500

    added flies

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,41 @@
 // Netflix & Grill
 
 // First HTML page: Main landing page
-    // Will have a nav bar on top with links to about/contact info, socials, etc
+// Will have a nav bar on top with links to about/contact info, socials, etc
 
-    // Background images (split?) showing something related to a theater/movie on one side and food on the other
+// Background images (split?) showing something related to a theater/movie on one side and food on the other
 
-    // At least two primary text inputs
+// At least two primary text inputs
 
-        // Movie genre
-        // Recipe ingredient or style
+// Movie genre
+// Recipe ingredient or style
 
-    // Try to have a button that then will return a random combo?
+// Try to have a button that then will return a random combo?
 
-    // The two primary inputs will need to be saved to local storage, and then those can be accessed by the second html page to display the movie & recipe info
+// The two primary inputs will need to be saved to local storage, and then those can be accessed by the second html page to display the movie & recipe info
 
-    // Footer with necessary info
+// Footer with necessary info
 
 // Second HTML page: Movie and Recipe cards/info
-    // Two cards, each will show title, image, maybe try for description/ingredients
+// Two cards, each will show title, image, maybe try for description/ingredients
 
-        // One for the movie result
-            // Should be to expand/open up a modal to show more details (cast, etc)
-        // One for the recipe result
-            // Should be to expand/open up a modal to show more details (list all steps and ingredients)
+// One for the movie result
+// Should be to expand/open up a modal to show more details (cast, etc)
+// One for the recipe result
+// Should be to expand/open up a modal to show more details (list all steps and ingredients)
 
-        // Need to access the local storage to grab the search input and make the API requests
+// Need to access the local storage to grab the search input and make the API requests
 
-        // We will need to go through the API requests and determine what info is obtainable/usable
-        
-        // Then append that usable info to the respective cards on the page
+// We will need to go through the API requests and determine what info is obtainable/usable
 
-    // Ability to re-roll search results
-        // First try to display the next result from the API object array
-        // If successful, try to randomize the re-roll result
+// Then append that usable info to the respective cards on the page
 
-    // Reference the API's as source of data
+// Ability to re-roll search results
+// First try to display the next result from the API object array
+// If successful, try to randomize the re-roll result
 
-    // Footer with necessary info
+// Reference the API's as source of data
+
+// Footer with necessary info
+
+console.log("connected");
