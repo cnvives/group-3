ad334b760a7b82ff22e00b027156b614fdc5f229
hkidd <harrisonakidd@gmail.com>
2021-07-07 13:16:14 -0500

    added variables for movie genres and dropdown selector.

--- assets/script.js
+++ assets/script.js
@@ -101,6 +101,8 @@
     // var war = warSel.value (10752);
     // var western = westernSel.value (37);
 
+    // 
+
     // fetch("https://salty-mountain-68764.herokuapp.com/https://api.themoviedb.org/3/discover/movie?api_key=1f0c12a5a877dc629a002fa2c6169442&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=35", {
     // })
     // .then(response => {
